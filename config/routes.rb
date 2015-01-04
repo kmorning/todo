e6f38a268f9406500d94bf2f810e027cfdd4e11f
Kelly Morning <kleemorning@gmail.com>
2015-01-04 16:56:36 -0500

    add todo_item resource and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :todo_items, only: [:index, :create, :update, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
