157e6340ed1f881e0c0269ddd9332f0964cf49d3
Kelly Morning <kleemorning@gmail.com>
2015-01-05 00:29:16 -0500

    fix styles, add create action invoked using ajax

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,18 @@
 class TodoItemsController < ApplicationController
   def index
+    @todo_items = TodoItem.all
+  end
+
+  def create
+    # The line below shows what params look like
+    #render plain: params[:todo_item].inspect
+    # Remember, we need to permit title because of strong parameters.
+    @todo_item = TodoItem.create(todo_item_params)
+  end
+
+  private
+  # method to permit parameter for todo_item
+  def todo_item_params
+    params.require(:todo_item).permit(:title)
   end
 end
